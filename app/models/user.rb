34db76f6f5a2088397feedbe54498b4ced4d17c7
ChiakiMori <s.zero.s2590@gmail.com>
2018-11-21 10:08:40 +0900

    EDIT group

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 has_many :massages
-has_many :groups_users
-has_many :groups,through: :groups_users
+has_many :group_users
+has_many :groups,through: :group_users
 
 end

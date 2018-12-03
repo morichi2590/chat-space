ab2c13c5a9866442adb4b14ad3b5ca75618da21f
ChiakiMori <s.zero.s2590@gmail.com>
2018-12-03 17:39:57 +0900

    EDIT message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,80 +1,80 @@
-// $(function () {
-//   function buildHTML(message){
-//     console.log(message)
-//     if (message.body == null && message.image == null) {
-//       return false;
-//     } else {
-//     var html = `<div class="message" data-message-id=${message.id}>
-//       <div class="message__name"> ${message.user_name} </div>
-//       <div class="message__time"> ${message.created_at} </div>
-//       <div class="message__clear"></div>
-//       <div class="message__text"> ${message.body} </div>
-//       </div>`;
-//     if (message.image == null){
-//       html = $(html).append(``)
-//     } else {
-//       html = $(html).append(`<img class="message__image" src="${message.image}" >`)
-//     }
-//     return html;
-//     }
-//   }
-//   $('#ms-form').on('submit',function(e) {
-//     e.preventDefault();
-//     var formData = new FormData(this);
-//     var href = window.location.href
-//     $.ajax({
-//       type: 'POST',
-//       url: href,
-//       data: formData,
-//       dataType: 'json',
-//       processData: false,
-//       contentType: false
-//     })
-//     .done(function(data){
-//       var ms = ".messages";
-//       if (buildHTML(data) == false) {
-//       $('.bottom__send').prop('disabled', false);
-//       } else {
-//       var html = buildHTML(data);
-//       $(ms).append(html)
-//       $('.bottom__text').val('')
-//       $('.hidden').val('')
-//       $('.bottom__send').prop('disabled', false);
-//       $(ms).animate({scrollTop: $(ms)[0].scrollHeight}, 'fast');
-//       }
-//     })
-//     .fail(function() {
-//       alert('error');
-//     });
-//   })
-//   setInterval(function() {
-//     if($('.message')[0]){
-//         var message_id = $('.message:last').data('message-id');
-//       } else {
-//         var message_id = 0
-//       }
-//           console.log(message_id)
-//     if (window.location.href.match(/\/groups\/\d+\/messages/)){
-//       $.ajax({
-//         type: 'GET',
-//         url: location.href,
-//         data: {
-//           message: { id: message_id }
-//         },
-//         dataType: 'json'
-//       })
-//       .done(function(data) {
-//         data.forEach(function(message) {
-//         var html = buildHTML(message);
-//         $('.messages').append(html);
-//         });
-//       })
-//       .fail(function(data) {
-//         alert('自動更新に失敗しました');
-//       })
-//     } else
-//     clearInterval(interval);
-//   },5000);
-// });
+$(function () {
+  function buildHTML(message){
+    console.log(message)
+    if (message.body == null && message.image == null) {
+      return false;
+    } else {
+    var html = `<div class="message" data-message-id=${message.id}>
+      <div class="message__name"> ${message.user_name} </div>
+      <div class="message__time"> ${message.created_at} </div>
+      <div class="message__clear"></div>
+      <div class="message__text"> ${message.body} </div>
+      </div>`;
+    if (message.image == null){
+      html = $(html).append(``)
+    } else {
+      html = $(html).append(`<img class="message__image" src="${message.image}" >`)
+    }
+    return html;
+    }
+  }
+  $('#ms-form').on('submit',function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var href = window.location.href
+    $.ajax({
+      type: 'POST',
+      url: href,
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var ms = ".messages";
+      if (buildHTML(data) == false) {
+      $('.bottom__send').prop('disabled', false);
+      } else {
+      var html = buildHTML(data);
+      $(ms).append(html)
+      $('.bottom__text').val('')
+      $('.hidden').val('')
+      $('.bottom__send').prop('disabled', false);
+      $(ms).animate({scrollTop: $(ms)[0].scrollHeight}, 'fast');
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  })
+  setInterval(function() {
+    if($('.message')[0]){
+        var message_id = $('.message:last').data('message-id');
+      } else {
+        var message_id = 0
+      }
+          console.log(message_id)
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      $.ajax({
+        type: 'GET',
+        url: location.href,
+        data: {
+          message: { id: message_id }
+        },
+        dataType: 'json'
+      })
+      .done(function(data) {
+        data.forEach(function(message) {
+        var html = buildHTML(message);
+        $('.messages').append(html);
+        });
+      })
+      .fail(function(data) {
+        alert('自動更新に失敗しました');
+      })
+    } else
+    clearInterval(interval);
+  },5000);
+});
 
 
